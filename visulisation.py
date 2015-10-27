7fb4e9fcceab49d5f24b6c5cd0bf47641d50a541
Ankit Agarwal <ankit.frnz@gmail.com>
2015-10-27 12:17:32 +0530

    Add visualization

--- visulisation.py
+++ visulisation.py
@@ -4,6 +4,8 @@ import scipy
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import mixture
 
 #########
 # Open a MySQL connection. Should be triggered by the caller before running
@@ -32,4 +34,26 @@ print(X_proj)
 first = X_proj.T[0].T
 second = X_proj.T[1].T
 plt.scatter(first, second, marker='o')
+plt.title("Raw data (scenes) with dimensions reduced to 2 using PCA")
+plt.show()
+
+# Let us do K-means now.
+km = KMeans(n_clusters=4)
+km.fit(X)
+labels = km.predict(X)
+colors = ['#000000', '#FFFFFF', '#FF0000', '#00FF00', '#0000FF']
+label_color = [colors[l] for l in labels]
+centroids = km.cluster_centers_
+plt.scatter(first, second, c=label_color)
+plt.title('K-means clustered data with dimensions reduced to 2 using PCA')
+plt.show()
+
+# GMM, here I come.
+mix = mixture.GMM(n_components=4, covariance_type='full')
+mix.fit(X)
+colors = ['#000000', '#FFFFFF', '#FF0000', '#00FF00', '#0000FF']
+labels = mix.predict(X)
+label_color = [colors[l] for l in labels]
+ax = plt.gca()
+ax.scatter(first, second, c=label_color, alpha=0.8)
 plt.show()
